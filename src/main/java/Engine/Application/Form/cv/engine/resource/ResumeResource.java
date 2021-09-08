f3fed348d575866acd1e956f7e6580718d2af78f
Faruque Braimo <faruquebraimo@gmail.com>
2021-09-08 23:58:25 +0200

    Files
    - get Single Resume
    - Open in browser
    - Open in file manager

--- src/main/java/Engine/Application/Form/cv/engine/resource/ResumeResource.java
+++ src/main/java/Engine/Application/Form/cv/engine/resource/ResumeResource.java
@@ -3,15 +3,24 @@ package Engine.Application.Form.cv.engine.resource;
 
 import Engine.Application.Form.cv.engine.message.ResponseMessage;
 import Engine.Application.Form.cv.engine.model.Resume;
-import Engine.Application.Form.cv.engine.model.UserEntity;
 import Engine.Application.Form.cv.engine.service.ResumeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -31,21 +40,45 @@ public class ResumeResource {
     }
 
     @PostMapping("/resumes/upload")
-    public ResponseEntity<ResponseMessage> uploadFiles(@RequestParam("files") MultipartFile[] files ) {
+    public ResponseEntity<ResponseMessage> uploadFiles(@RequestParam("files") MultipartFile[] files) {
         String message = "";
         try {
             List<String> fileNames = new ArrayList<>();
 
             Arrays.asList(files).stream().forEach(file -> {
-                resumeService.saveResumes(file, "desenvolvedor-java-junio");
+                resumeService.saveResumes(file, "desenvolvedor-java-junior");
                 fileNames.add(file.getOriginalFilename());
             });
 
-            message = "Houve sucesso no carregado de" + fileNames ;
+            message = "Houve sucesso no carregado de" + fileNames;
             return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
         } catch (Exception e) {
             message = "Falha ao carregar!";
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
         }
+    }
+
+
+    @GetMapping("/openResume/{jobFolder}/{resumeName}")
+    public ResponseEntity<InputStreamResource> getTermsConditions(@PathVariable String jobFolder, @PathVariable String resumeName) throws FileNotFoundException {
+        Path currentWorkingDir = Paths.get("").toAbsolutePath();
+        Path root = Paths.get(currentWorkingDir.normalize().toString() + "/resumes/" + jobFolder + "/" + resumeName);
+        File file = new File(root.toAbsolutePath().toString());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("content-disposition", "inline;filename=" + file.getName());
+
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/pdf"))
+                .body(resource);
+    }
+
+    @GetMapping("/locateResume/{jobFolder}/{resumeName}")
+    public ResponseEntity<String> locateFile(@PathVariable String jobFolder, @PathVariable String resumeName) throws URISyntaxException, IOException {
+        resumeService.locateFile(jobFolder, resumeName);
+        return new ResponseEntity<>("Localizado!", HttpStatus.OK);
+
     }
 }
--- src/main/java/Engine/Application/Form/cv/engine/service/ResumeService.java
+++ src/main/java/Engine/Application/Form/cv/engine/service/ResumeService.java
@@ -2,16 +2,25 @@ package Engine.Application.Form.cv.engine.service;
 
 import Engine.Application.Form.cv.engine.model.Resume;
 import Engine.Application.Form.cv.engine.resume.ResumeDatabase;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 @Service
@@ -35,17 +44,61 @@ public class ResumeService {
         return resumeDatabase.loadResume(job);
     }
 
+
+    public File getResumeFile(String resumeName, String job_folder) {
+
+        File result = null;
+        try {
+
+            Path root = Paths.get(currentWorkingDir.normalize().toString() + "/resumes/" + job_folder);
+            Path file = root.resolve(resumeName);
+            Resource resource = new UrlResource(file.toUri());
+            result = resource.getFile();
+
+            Files.copy(resource.getInputStream(), root, StandardCopyOption.REPLACE_EXISTING);
+
+
+            if (resource.exists() || resource.isReadable()) {
+            } else {
+                throw new RuntimeException("Falha ao ler o Cv!");
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Erro: " + e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
     public void deleteResume(Resume resume) throws IOException {
         Path root = Paths.get(resume.getPath());
         Files.delete(root);
     }
 
+
+    public  void locateFile(String job_folder, String  resumeName) throws URISyntaxException, IOException {
+        Path root = Paths.get(currentWorkingDir.normalize().toString() + "/resumes/" + job_folder + "/" + resumeName);
+        File file = new File(root.toAbsolutePath().toString());
+
+        try {
+            Runtime.getRuntime().exec("explorer.exe  /select," + file.getAbsolutePath());
+        } catch (Exception ex) {
+            System.out.println("Error - " + ex);
+        }
+    }
+
+
     public void deleteAllResume(String job_folder) {
         Path root = Paths.get(currentWorkingDir.normalize().toString() + "/resumes/" + job_folder + "/");
         FileSystemUtils.deleteRecursively(root.toFile());
+
     }
 
     public void createDirectory(String name) throws IOException {
         resumeDatabase.createDirectory(name);
+    }
+
+    public static void main(String[] args) throws URISyntaxException, IOException {
     }
 }
